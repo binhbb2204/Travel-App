2d910f270acb947038779a2a542a67fc63f4ccaa
mthn.04 <minhthuan24062004@gmail.com>
2024-11-12 23:42:58 +0700

    minor updates

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import {Routes, Route, Navigate} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../page/Home'
 import About from '../page/About'
-import Tours from '../page/Tours' 
-import TourDetails from '../page/TourDetails' 
-import SearchResultList from '../page/SearchResultList' 
-import Login from '../page/Login' 
-import Register from '../page/Register' 
+import Tours from '../page/Tours'
+import TourDetails from '../page/TourDetails'
+import SearchResultList from '../page/SearchResultList'
+import Login from '../page/Login'
+import Register from '../page/Register'
 import Location from '../page/Location'
 const Routers = () => {
   return (
     <Routes>
-        <Route path = '/' element = {<Navigate to = '/home'/>}/>
-        <Route path = '/home' element = {<Home/>}  />
-        <Route path='/about' element={<About />} />
-        <Route path = '/tours' element = {<Tours/>}  />
-        <Route path = '/tours/:id' element = {<TourDetails/>}  />
-        <Route path = '/login' element = {<Login/>}  />
-        <Route path = '/register' element = {<Register/>}  />
-        <Route path = '/tours/search' element = {<SearchResultList/>}  />
-        <Route path = '/exotic_tours' element={<Location/>} />
+      <Route path='/' element={<Navigate to='/home' />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/tours' element={<Tours />} />
+      <Route path='/tours/:id' element={<TourDetails />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/tours/search' element={<SearchResultList />} />
+      <Route path='/exotic_tours' element={<Location />} />
 
     </Routes>
   )
