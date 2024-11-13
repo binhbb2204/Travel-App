4f5b80a7a8bb2b63f4f67cd0800ea5f236d7b6e5
Hoang Thien An <anhoang17935@gmail.com>
2024-11-13 17:41:56 +0700

    Necessary update

--- frontend/src/page/About.jsx
+++ frontend/src/page/About.jsx
@@ -6,8 +6,8 @@ import teamMemberImage1 from '../images/binhbb.jpg';
 import teamMemberImage2 from '../images/ice.jpg';
 import teamMemberImage3 from '../images/thuan.jpg';
 import backgroundImage from '../images/nature_2.jpg';
-import aboutVideo from '../images/home-bg-final_2.mp4';
-
+// import aboutVideo from '../images/home-bg-final_2.mp4';
+import aboutVideo from '../images/AboutUs.mp4';
 import AirAsia from '../images/logo/AirAsia.jpg';
 import QatarAirways from '../images/logo/QatarAirways.jpg';
 import Trivago from '../images/logo/Trivago.jpg';
