9c621ea537c6ecf4f76e73e8a81b7763458fdc0c
binhbb2204 <binhbb2204@gmail.com>
2024-11-13 15:41:54 +0700

    wots diz?

---
+++ src/ui/Card/TourSearchCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Search } from 'lucide-react'
+import { Card, CardContent } from './Card'
+const TourSearchCard = () => {
+  return (
+    <div>TourSearchCard</div>
+  )
+}
+
+export default TourSearchCard
