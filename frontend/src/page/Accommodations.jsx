8882b0bf81a55400e22c2d1cd84d0d34198ec4e4
Hoang Thien An <anhoang17935@gmail.com>
2024-11-20 14:42:26 +0700

    Added transportation and accommodation environment and added to header

---
+++ frontend/src/page/Accommodations.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
+import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
+import { Link } from 'react-router-dom';
+import '../styles/accomodations.css';
+
+const Accommodations = () => {
+    return (
+    <Container className='acco__container'>
+        <section>
+            Accomodations
+        </section>
+    </Container>   
+    );
+};  
+
+export default Accommodations;
---
+++ frontend/src/page/Transportations.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
+import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
+import { Link } from 'react-router-dom';
+import '../styles/transportations.css';
+
+const Transportations = () => {
+    return (
+    <Container className='trans__container'>
+        <section>
+            Transportation
+        </section>
+    </Container>
+    );
+};
+
+export default Transportations;
