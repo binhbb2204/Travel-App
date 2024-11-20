23e1a73e81119449340d99ab4c0777a9523c7422
Hoang Thien An <anhoang17935@gmail.com>
2024-11-20 21:12:48 +0700

    Update accommodation early building

--- frontend/src/page/Accommodations.jsx
+++ frontend/src/page/Accommodations.jsx
@@ -3,13 +3,21 @@ import { motion } from 'framer-motion';
 
 import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
 import { Link } from 'react-router-dom';
+import AccomodationSearchBox from '../ui/SearchBar/AccommodationSearchBox';
 import '../styles/accomodations.css';
 
 const Accommodations = () => {
     return (
     <Container className='acco__container'>
-        <section>
-            Accomodations
+        <section className='search__container'>
+            <div className="search__box">
+                <AccomodationSearchBox>
+                    testing
+                </AccomodationSearchBox>
+            </div>
+        </section>
+        <section className='result__container'>
+
         </section>
     </Container>   
     );
