15c91f822d8f03d1e3d25ce51d14106a4a0663e2
mthn.04 <minhthuan24062004@gmail.com>
2024-11-30 18:46:49 +0700

    (admin) users panel (uncompleted)

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -4,14 +4,14 @@ import bcrypt from "bcryptjs";
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.accessToken
 
-    if(!token){
-        return res.status(401).json({success: false, message: 'You are not authorized'})
+    if (!token) {
+        return res.status(401).json({ success: false, message: 'You are not authorized' })
     }
 
     //if token exists then verify the token
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user)=>{
-        if(err){
-            return res.status(401).json({success: false, message: 'Token is invalid'})
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(401).json({ success: false, message: 'Token is invalid' })
         }
 
         req.user = user
@@ -20,23 +20,23 @@ export const verifyToken = (req, res, next) => {
 }
 
 export const verifyUser = (req, res, next) => {
-    verifyToken(req, res, next, ()=> {
-        if(req.user.id === req.params.id || req.user.role === 'admin'){
+    verifyToken(req, res, next, () => {
+        if (req.user.id === req.params.id || req.user.role === 'admin') {
             next()
         }
-        else{
-           return res.status(401).json({success: false, message: 'You are not authenticated'})
+        else {
+            return res.status(401).json({ success: false, message: 'You are not authenticated' })
         }
     })
 }
 
 export const verifyAdmin = (req, res, next) => {
-    verifyToken(req, res, next, ()=> {
-        if(req.user.role === 'admin'){
+    verifyToken(req, res, next, () => {
+        if (req.user.role === 'admin') {
             next()
         }
-        else{
-           return res.status(401).json({success: false, message: 'You are not authorized'})
+        else {
+            return res.status(401).json({ success: false, message: 'You are not authorized' })
         }
     })
 }
