f0fe9e9b6a245d76218c43d9f6959173e3abbee6
binhbb2204 <binhbb2204@gmail.com>
2024-12-05 08:56:04 +0700

    saved

--- src/data/Service/authService.js
+++ src/data/Service/authService.js
@@ -4,10 +4,10 @@ const BASE_URL = 'http://localhost:8000/api/v1/auth';
 export const authService = {
     login: async (email, password) => {
         try {
-            const response = await axios.post( `${BASE_URL}/login`, { email, password });
+            const response = await axios.post(`${BASE_URL}/login`, { email, password });
             const { token, data, role } = response.data;
             // Assuming the backend returns a token
-            
+
             localStorage.setItem('token', token);
             localStorage.setItem('userId', data._id);
             localStorage.setItem('username', data.name);
@@ -23,16 +23,29 @@ export const authService = {
 
     logout: () => {
         // Remove all user-related items from localStorage
+<<<<<<< HEAD
+        const keysToRemove = [
+            'token', 
+            'userId', 
+            'username', 
+            'userEmail', 
+            'userRole', 
+        ];
+    
+        keysToRemove.forEach(key => localStorage.removeItem(key));
+        
+=======
         localStorage.removeItem('token');
         localStorage.removeItem('userId');
         localStorage.removeItem('username');
         localStorage.removeItem('userEmail');
         localStorage.removeItem('userRole');
-        
+
         // Clear all items related to "Remember Me" functionality
         localStorage.removeItem('email');
         localStorage.removeItem('password');
         localStorage.removeItem('rememberMe');
+>>>>>>> cf33d2868f34488d522110f056294aa261b21ea5
     },
 
     getCurrentUser: () => {
