8406488813bcfcf5c453ebe4e0aeabf105b29032
binhbb2204 <binhbb2204@gmail.com>
2024-12-06 01:40:51 +0700

    fix the rating

--- frontend/src/utils/avgRating.js
+++ frontend/src/utils/avgRating.js
@@ -1,11 +1,19 @@
 const calculateAvgRating = reviews => {
+    
+
+    const ratedReviews = reviews.filter(item => item.rating !== null && item.rating > 0);
+
     const totalRating = reviews?.reduce((acc, item)=> acc + item.rating, 0)
     const avgRating = 
-      totalRating === 0 
-        ? " " 
-        : totalRating === 1 
-        ? totalRating 
-        : totalRating/ reviews ?.length;
+        ratedReviews.length > 0 
+        ? (totalRating / ratedReviews.length).toFixed(1) 
+        : 0;
+    // const avgRating = 
+    //   totalRating === 0 
+    //     ? " " 
+    //     : totalRating === 1 
+    //     ? totalRating 
+    //     : totalRating/ reviews ?.length;
 
     return {
         totalRating,
