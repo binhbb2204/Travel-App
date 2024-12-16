c35c67325cf9fb4afcff33b517e8304423c1575d
mthn.04 <minhthuan24062004@gmail.com>
2024-12-16 14:31:36 +0700

    save

--- backend/Config/uploadAccommodationMiddleware.js
+++ backend/Config/uploadAccommodationMiddleware.js
@@ -6,7 +6,7 @@ const accommodationStorage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'accommodations', // Separate folder for accommodations
-    allowed_formats: ['jpg', 'jpeg', 'png'], 
+    allowed_formats: ['jpg', 'jpeg', 'png', 'webp'], 
     public_id: (req, file) => `accommodation_${Date.now()}_${file.originalname}`,
   },
 });
